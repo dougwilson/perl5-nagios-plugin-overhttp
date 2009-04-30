daf42bd3157bc7e3ba0894d3d669a3987bb7d891
Douglas Christopher Wilson <dougwilson@live.com>
2009-04-30 12:34:21 -0400

    Edited the default template to my own style.

--- lib/Nagios/Plugin/OverHTTP.pm
+++ lib/Nagios/Plugin/OverHTTP.pm
@@ -1,62 +1,51 @@
 package Nagios::Plugin::OverHTTP;
 
 use 5.008;
-use warnings;
 use strict;
+use utf8;
+use version 0.74;
+use warnings 'all';
+
+# Module metadata
+our $AUTHORITY = 'cpan:DOUGDUDE';
+our $VERSION = '0.01';
+
+1;
+
+__END__
 
 =head1 NAME
 
-Nagios::Plugin::OverHTTP - The great new Nagios::Plugin::OverHTTP!
+Nagios::Plugin::OverHTTP - Nagios plugin to check over the HTTP protocol.
 
 =head1 VERSION
 
 Version 0.01
 
-=cut
-
-our $VERSION = '0.01';
-
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+  my $plugin = Nagios::Plugin::OverHTTP->new(
+      url => 'https://myserver.net/nagios/check_some_service.cgi',
+  );
 
-Perhaps a little code snippet.
+  my $status  = $plugin->status;
+  my $message = $plugin->message;
 
-    use Nagios::Plugin::OverHTTP;
+=head1 DESCRIPTION
 
-    my $foo = Nagios::Plugin::OverHTTP->new();
-    ...
+This Nagios plugin provides a way to check services remotely over the HTTP
+protocol.
 
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+=head1 DEPENDENCIES
 
 =head1 AUTHOR
 
 Douglas Christopher Wilson, C<< <doug at somethingdoug.com> >>
 
-=head1 BUGS
+=head1 BUGS AND LIMITATIONS
 
-Please report any bugs or feature requests to C<bug-nagios-plugin-overhttp at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Nagios-Plugin-OverHTTP>.  I will be notified, and then you'll
+Please report any bugs or feature requests to C<bug-authen-cas-external at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Authen-CAS-External>. I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
 
 
@@ -66,7 +55,7 @@ automatically be notified of progress on your bug as I make changes.
 
 You can find documentation for this module with the perldoc command.
 
-    perldoc Nagios::Plugin::OverHTTP
+perldoc Authen::CAS::External
 
 
 You can also look for information at:
@@ -75,19 +64,19 @@ You can also look for information at:
 
 =item * RT: CPAN's request tracker
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Nagios-Plugin-OverHTTP>
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Authen-CAS-External>
 
 =item * AnnoCPAN: Annotated CPAN documentation
 
-L<http://annocpan.org/dist/Nagios-Plugin-OverHTTP>
+L<http://annocpan.org/dist/Authen-CAS-External>
 
 =item * CPAN Ratings
 
-L<http://cpanratings.perl.org/d/Nagios-Plugin-OverHTTP>
+L<http://cpanratings.perl.org/d/Authen-CAS-External>
 
 =item * Search CPAN
 
-L<http://search.cpan.org/dist/Nagios-Plugin-OverHTTP/>
+L<http://search.cpan.org/dist/Authen-CAS-External/>
 
 =back
 
@@ -95,13 +84,9 @@ L<http://search.cpan.org/dist/Nagios-Plugin-OverHTTP/>
 =head1 ACKNOWLEDGEMENTS
 
 
-=head1 COPYRIGHT & LICENSE
+=head1 LICENSE AND COPYRIGHT
 
 Copyright 2009 Douglas Christopher Wilson, all rights reserved.
 
-This program is released under the following license: gpl
-
-
-=cut
-
-1; # End of Nagios::Plugin::OverHTTP
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
