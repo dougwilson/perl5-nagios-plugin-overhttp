bfbae88ee0b8bca759ba50fe53451f2e6a90e2b2
Douglas Christopher Wilson <dougwilson@live.com>
2009-04-30 11:35:22 -0400

    Adding Module::Starter framework

---
+++ lib/Nagios/Plugin/OverHTTP.pm
@@ -0,0 +1,106 @@
+package Nagios::Plugin::OverHTTP;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Nagios::Plugin::OverHTTP - The great new Nagios::Plugin::OverHTTP!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Nagios::Plugin::OverHTTP;
+
+    my $foo = Nagios::Plugin::OverHTTP->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Douglas Christopher Wilson, C<< <doug at somethingdoug.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-nagios-plugin-overhttp at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Nagios-Plugin-OverHTTP>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Nagios::Plugin::OverHTTP
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Nagios-Plugin-OverHTTP>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Nagios-Plugin-OverHTTP>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Nagios-Plugin-OverHTTP>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Nagios-Plugin-OverHTTP/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Douglas Christopher Wilson, all rights reserved.
+
+This program is released under the following license: gpl
+
+
+=cut
+
+1; # End of Nagios::Plugin::OverHTTP
